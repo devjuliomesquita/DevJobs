35f57a7074e9b4650e6725532d20ecfa9720e9d4
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-05-15 18:44:16 -0300

    Criado os contratos de Repositorio e Serviços

---
+++ Devjobs.Core/Interfaces/Repository/IRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using DevJobs.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevJobs.Core.Interfaces.Repository
+{
+    public interface IRepositoryBase<TEntity> where TEntity : BaseEntity
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        IList<TEntity> GetAll();
+        TEntity GetById(int id);
+    }
+}
---
+++ Devjobs.Core/Interfaces/Service/IServiceBase.cs
@@ -0,0 +1,25 @@
+﻿using DevJobs.Core.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevJobs.Core.Interfaces.Service
+{
+    public interface IServiceBase<TEntity> where TEntity : BaseEntity
+    {
+        TEntity Add<TInputModel, TOutputModel, TValidator>(TInputModel inputModel) 
+            where TValidator : AbstractValidator<TEntity>
+            where TInputModel : class
+            where TOutputModel : class;
+        TEntity Update<TInputModel, TOutputModel, TValidator>(TInputModel inputModel) 
+            where TValidator : AbstractValidator<TEntity>
+            where TInputModel : class
+            where TOutputModel : class;
+        void Delete(int id);
+        IEnumerable<TOutputModel>GetAll<TOutputModel>() where TOutputModel : class;
+        TOutputModel GetById<TOutputModel>(int id) where TOutputModel : class;
+    }
+}
