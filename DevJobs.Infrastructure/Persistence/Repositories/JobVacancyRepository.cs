d91dee18be7e2a2b93b8349acf9b58a1166b9b4a
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-05-15 22:40:12 -0300

    Criado a interface e implementação do JobVacancy

---
+++ DevJobs.Infrastructure/Persistence/Repositories/JobVacancyRepository.cs
@@ -0,0 +1,26 @@
+﻿using DevJobs.Core.Entities;
+using DevJobs.Core.Interfaces.Repository;
+using DevJobs.Infrastructure.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevJobs.Infrastructure.Persistence.Repositories
+{
+    public class JobVacancyRepository : RepositoryBase<JobVacancy>, IJobVacancyRepository 
+    {
+        private readonly DevJobsContext _context;
+        public JobVacancyRepository(DevJobsContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void AddApplication(JobApplication jobApplication)
+        {
+            _context.Set<JobApplication>().Add(jobApplication);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Devjobs.Core/Interfaces/Repository/IJobVacancyRepository.cs
@@ -0,0 +1,14 @@
+﻿using DevJobs.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevJobs.Core.Interfaces.Repository
+{
+    public interface IJobVacancyRepository : IRepositoryBase<JobVacancy> 
+    {
+        void AddApplication(JobApplication jobApplication);
+    }
+}
