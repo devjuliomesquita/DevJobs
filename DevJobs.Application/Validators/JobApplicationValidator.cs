c41ce079dae0ac5d4f5a75dabc9b32cc41739dc4
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-05-16 00:03:56 -0300

    Criado a validação com o fluent validator

---
+++ DevJobs.Application/Validators/JobApplicationValidator.cs
@@ -0,0 +1,26 @@
+﻿
+using DevJobs.Core.Entities;
+using FluentValidation;
+
+
+namespace DevJobs.Application.Validators
+{
+    public class JobApplicationValidator : AbstractValidator<JobApplication>
+    {
+        public JobApplicationValidator()
+        {
+            RuleFor(ja => ja.ApplicatName)
+                .NotEmpty().WithMessage("Por favor entre com um nome.")
+                .NotNull().WithMessage("Por favor entre com um nome.")
+                .MaximumLength(150).WithMessage("Nome pode conter no máximo 150 caracteres.")
+                .MinimumLength(3).WithMessage("Nome deve conter no mínimo 3 caractes.");
+            RuleFor(ja => ja.ApplicantEmail)
+                .NotEmpty().WithMessage("Por favor entre com um e-mail.")
+                .NotNull().WithMessage("Por favor entre com um e-mail.")
+                .MaximumLength(100).WithMessage("E-mail pode conter no máximo 100 caracteres.")
+                .EmailAddress().WithMessage("Por favor digite um e-mail válido.");
+
+
+        }
+    }
+}
---
+++ DevJobs.Application/Validators/JobVacancyValidator.cs
@@ -0,0 +1,37 @@
+﻿using DevJobs.Core.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevJobs.Application.Validators
+{
+    public class JobVacancyValidator : AbstractValidator<JobVacancy>
+    {
+        public JobVacancyValidator()
+        {
+            RuleFor(jv => jv.Title)
+                .NotEmpty().WithMessage("Por favor entre com um título.")
+                .NotNull().WithMessage("Por favor entre com um título.")
+                .MaximumLength(100).WithMessage("Título pode conter no máximo 150 caracteres.")
+                .MinimumLength(30).WithMessage("Título deve conter no mínimo 30 caractes.");
+            RuleFor(jv => jv.Description)
+                .NotEmpty().WithMessage("Por favor entre com um descrição.")
+                .NotNull().WithMessage("Por favor entre com um descrição.")
+                .MaximumLength(300).WithMessage("A descrição pode conter no máximo 150 caracteres.")
+                .MinimumLength(30).WithMessage("A descrição deve conter no mínimo 30 caractes.");
+            RuleFor(jv => jv.Company)
+                .NotEmpty().WithMessage("Por favor entre com um companhia.")
+                .NotNull().WithMessage("Por favor entre com um companhia.")
+                .MaximumLength(100).WithMessage("Nome da companhia pode conter no máximo 150 caracteres.")
+                .MinimumLength(30).WithMessage("Nome da companhia deve conter no mínimo 30 caractes.");
+            RuleFor(jv => jv.SalaryRange)
+                .NotEmpty().WithMessage("Por favor entre com um range salarial.")
+                .NotNull().WithMessage("Por favor entre com um range salarial.")
+                .MaximumLength(50).WithMessage("O range salarial pode conter no máximo 150 caracteres.")
+                .MinimumLength(4).WithMessage("O range salarial deve conter no mínimo 4 caractes.");
+        }
+    }
+}
